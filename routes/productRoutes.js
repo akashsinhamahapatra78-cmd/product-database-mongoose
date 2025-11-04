0456950e5ccbdb3ab6a717b4cce7f93c7f1ba003
akashsinhamahapatra78-cmd <akashsinhamahapatra78@gmail.com>
2025-11-04 14:01:11 +0530

    Create productRoutes.js

---
+++ routes/productRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+
+// Create a new product
+router.post('/', productController.createProduct);
+
+// Get all products
+router.get('/', productController.getAllProducts);
+
+// Get a single product by ID
+router.get('/:id', productController.getProductById);
+
+// Update a product
+router.put('/:id', productController.updateProduct);
+
+// Delete a product
+router.delete('/:id', productController.deleteProduct);
+
+// Search products
+router.get('/search/query', productController.searchProducts);
+
+module.exports = router;
